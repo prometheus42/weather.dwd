efa2b4a1be9ec81f83874d79f45d13abb3147d5d
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2023-12-01 19:49:49 +0100

    Improved addon and added settings.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # weather.dwd - A Kodi Plugin for the German Weather Service (DWD) weather forecasts
 
-This Kodi plugin fetches weather reports from the National Weather Service (NWS) (eg NOAA) from their https://www.weather.gov public api feed
+This Kodi plugin fetches weather reports from the German Weather Service (DWD)
+provided for the WarnWetter app. Note that the DWD only provides weather
+forecasts for Germany.
 
-Note the DWD only provides weather forecasts for Germany.
+All weather stations for which information can be obtained can be found in this
+list: https://www.dwd.de/DE/leistungen/met_verfahren_mosmix/mosmix_stationskatalog.cfg
 
 ## Links
 * This plugin is based on the NOAA plugin: https://github.com/randallspicher/weather.noaa/
-* https://kodi.wiki/view/Add-on_development
+* Some code was taken from: https://github.com/vlmaksime/weather.gismeteo/
+* All information about developing Kodi addons: https://kodi.wiki/view/Add-on_development
 * Kodi wiki entry for developing weather plugins: https://kodi.wiki/view/Weather_addons
-* https://kodi.wiki/view/Add-on_settings
-* https://github.com/bundesAPI/dwd-api
-* 
+* Info for settings in Kodi addons: https://kodi.wiki/view/Add-on_settings
+* OpenAPI for the weather API of the DWD: https://github.com/bundesAPI/dwd-api
--- default.py
+++ default.py
@@ -1,6 +1,8 @@
 
 import os
 import sys
+import csv
+import json
 
 import requests
 
@@ -26,7 +28,7 @@ DATEFORMAT = xbmc.getRegion('dateshort')
 TIMEFORMAT = xbmc.getRegion('meridiem')
 
 MAXDAYS = 10
-STATION_ID = 'E626'
+TIMEOUT = 10
 API_URL = 'https://app-prod-ws.warnwetter.de/v30/stationOverviewExtended?stationIds={station}'
 
 ICON_MAPPING = {
@@ -116,6 +118,7 @@ WEATHER_ICON_MAPPING = {
     "na": 'not available',
 }
 
+
 def get_wind_direction(deg):
     """
     Convert degrees on the compass to the correct label.
@@ -155,29 +158,16 @@ def get_wind_direction(deg):
     elif deg >= 327 and deg <= 348:
         return 86
 
+
 def log(txt, level=xbmc.LOGDEBUG):
     """
     Log a message with a given log level.
 
     Source: https://github.com/randallspicher/weather.noaa/blob/master/resources/lib/utils.py
     """
-    if DEBUG:
+    if True:
         message = f'{ADDONID}: {txt}'
         xbmc.log(msg=message, level=level)
-
-
-def fetch_location(num):
-    """Fetch list of stations and set station name from ID."""
-    log(f'Fetching Location name for ID: {num}')
-    list_of_stations = {
-        8040: 'Woanders',
-        8041: 'Bramsche',
-        8042: 'Keine-Ahnung',
-    }
-    prefix = "Location"+num
-    station_id = ADDON.getSetting(prefix+'ID')
-    location_name = list_of_stations[station_id]
-    ADDON.setSetting(prefix, location_name)
 
 
 def set_property(name, value):
@@ -211,35 +201,88 @@ def clear():
         set_property(f'Day{count}.FanartCode', 'na')
 
 
+def div10(text):
+    try:
+        return int(text) / 10.
+    except ValueError:
+        return 0.0
+
+
+def getFeelsLike(T=10, V=25):
+    """ The formula to calculate the equivalent temperature related to the wind chill is:
+        T(REF) = 13.12 + 0.6215 * T - 11.37 * V**0.16 + 0.3965 * T * V**0.16
+        Or:
+        T(REF): is the equivalent temperature in degrees Celsius
+        V: is the wind speed in km/h measured at 10m height
+        T: is the temperature of the air in degrees Celsius
+        source: http://zpag.tripod.com/Meteo/eolien.htm
+        source: https://forum.kodi.tv/showthread.php?tid=114637&pid=937168#pid937168
+    """
+    FeelsLike = T
+    # Wind speeds of 4 mph or less, the wind chill temperature is the same as the actual air temperature.
+    if round((V + .0) / 1.609344) > 4:
+        FeelsLike = (13.12 + (0.6215 * T) -
+                     (11.37 * V**0.16) + (0.3965 * T * V**0.16))
+    return str(round(FeelsLike))
+
+
+def get_station_ids():
+    station_ids = [ADDON.getSetting('Location1ID'), ADDON.getSetting(
+        'Location2ID'), ADDON.getSetting('Location3ID'), ADDON.getSetting('Location4ID')]
+    station_ids = list(filter(None, station_ids))
+    log(f'Stations for which to fetch data: {station_ids}')
+    return station_ids
+
+
+def get_station_names():
+    station_names = [ADDON.getSetting('Location1Name'), ADDON.getSetting(
+        'Location2Name'), ADDON.getSetting('Location3Name'), ADDON.getSetting('Location4Name')]
+    station_names = list(filter(None, station_names))
+    return station_names
+
+
 def fetch_weather_data():
     """
-    Fetches weather data from DWD and sets properties for next 10 days???.
+    Fetches weather data from DWD for all given station IDs.
     """
-    r = requests.get(API_URL.format(station=STATION_ID), timeout=10)
-    weather_data, = r.json().values()
-    current_data = weather_data["forecast1"]
-    set_property('Current.Condition',
-                 ICON_MAPPING[current_data["icon"][0]])  # 'icon1h'
-    # if temperatur == 32767: pass
-    set_property('Current.Temperature', current_data["temperature"][0])
-    set_property('Current.Wind', '0')  # 'windSpeed'
-    set_property('Current.WindDirection', 'N/A')  # 'windDirection'
-    set_property('Current.Humidity', int(current_data['humidity'][0])/10)
+    log('Fetching weather info...')
+    station_ids = get_station_ids()
+    r = requests.get(API_URL.format(
+        station=','.join(station_ids)), timeout=TIMEOUT)
+    for weather_data in r.json().values():
+        marshal_weather_data(weather_data)
+        marshal_alert_data(weather_data)
+        # TODO: Check how to support multiple locations.
+        break
+
+
+def marshal_weather_data(weather_data):
+    """
+    Set properties for a single weather station for today and the next days.
+    """
+    log(f'Marshal weather data for {weather_data["forecast1"]["stationId"]}')
+    current_data = weather_data['forecast1']
+    set_property('Current.Condition', ICON_MAPPING[current_data['icon1h'][0]])
+    set_property('Current.Temperature', current_data['temperature'][0])
+    set_property('Current.Humidity', div10(current_data['humidity'][0]))
     set_property('Current.ChancePrecipitation',
                  current_data['precipitationProbablity'])  # precipitationTotal
-    set_property('Current.FeelsLike', '0')
-    set_property('Current.UVIndex', '0')
-    set_property('Current.DewPoint', int(current_data['dewPoint2m'][0])/10)
+    set_property('Current.DewPoint', div10(current_data['dewPoint2m'][0]))
     set_property('Current.OutlookIcon', 'na.png')
     set_property('Current.WindGust', current_data['windGust'])
-    # set_property('Current.SeaLevel'	, '')
-    # set_property('Current.GroundLevel'	, '')
     set_property('Current.FanartCode', 'na')
     # TODO: Check whether to include: sunshine, surfacePressure, isDay.
     # use wind information from first day (today!), because it is missing in the 'current' data
-    set_property('Current.Wind', weather_data['days'][0]['windSpeed'])
-    wind_direction = int(weather_data['days'][0]['windDirection']) / 10
-    set_property('Current.WindDirection', xbmc.getLocalizedString(get_wind_direction(wind_direction)))
+    set_property('Current.Wind', div10(weather_data['days'][0]['windSpeed']))
+    wind_direction = div10(weather_data['days'][0]['windDirection'])
+    set_property('Current.WindDirection', xbmc.getLocalizedString(
+        get_wind_direction(wind_direction)))
+    # calculate feels like temperature
+    set_property('Current.FeelsLike',  getFeelsLike(
+        current_data["temperature"][0], div10(weather_data['days'][0]['windSpeed'])))
+    # set_property('Current.UVIndex', '0')
+    # set_property('Current.SeaLevel'	, '')
+    # set_property('Current.GroundLevel'	, '')
     #
     for count, day in enumerate(weather_data['days'][1:]):
         set_property(f'Day{count}.Title', day['dayDate'])
@@ -248,8 +291,9 @@ def fetch_weather_data():
         # set_property('Daily.%i.TempDay' % (count+1), u'%s%s' % (FtoC(item['temperature']), TEMPUNIT))
         # set_property('Daily.%i.TempNight'	% (count+1), '')
         set_property(f'Day{count}.Outlook', ICON_MAPPING[day['icon']])
-        # set_property('Daily.%i.WindDirection'	% (count+1), item['windDirection'])
-        # set_property('Daily.%i.WindSpeed'	% (count+1), item['windSpeed'])
+        set_property(f'Daily.{count}.WindDirection', xbmc.getLocalizedString(
+            get_wind_direction(day['windDirection'])))
+        set_property(f'Daily.{count}.WindSpeed', day['windSpeed'])
         # set_property('Daily.%i.ShortOutlook'	% (count+1), item['shortForecast'])
         # set_property('Daily.%i.DetailedOutlook'	% (count+1), item['detailedForecast'])
         # 		code, rain=code_from_icon(icon)
@@ -264,12 +308,11 @@ def fetch_weather_data():
         set_property(f'Daily.{count+1}.Precipitation', day['precipitation'])
 
 
-def fetch_weather_alerts():
+def marshal_alert_data(weather_data):
     """
-    Fetches any weather alerts for location.
+    Marshal any weather alerts for a given location.
     """
-    r = requests.get(API_URL.format(station=STATION_ID), timeout=10)
-    weather_data, = r.json().values()
+    log(f'Marshal alert data for {weather_data["forecast1"]["stationId"]}')
     warning_data = weather_data['warnings']
     if warning_data:
         set_property('Alerts.IsFetched', 'true')
@@ -294,29 +337,99 @@ def fetch_weather_alerts():
             # set_property('Alerts.%i.response'	% (count+1), str(thisdata['response']))
     else:
         clear_property('Alerts.IsFetched')
-        xbmc.log('No current weather alerts.', level=xbmc.LOGDEBUG)
+        log('No current weather alerts.', level=xbmc.LOGDEBUG)
+
+
+def check_station_list(station_list):
+    new_station_list = []
+    for s in station_list:
+        r = requests.get(API_URL.format(station=s[0]), timeout=10)
+        if r.json():
+            new_station_list.append(s)
+    return new_station_list
+
+
+def load_station_list():
+    filename = xbmcvfs.translatePath(os.path.join(
+        ADDON.getAddonInfo('path'), 'resources', 'station_list.csv'))
+    station_list = []
+    with open(filename, newline='', encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            # TODO: Find a better station list without mojibake! -----v
+            station_list.append(
+                (row['ID'], row['NAME'].replace(chr(65533), '').title()))
+    return station_list
+
+
+def find_station_by_name(station_name):
+    station_list = load_station_list()
+    # TODO: Check whether to use SequenceMatcher from difflib or other algorithms like Levenshtein Distance.
+    station_list = [s for s in station_list if station_name in s[1]]
+    return check_station_list(station_list)
+
+
+def find_location(location_no):
+    """
+    Find all locations from a station list of the DWD by searching for a keyword.
+    """
+    keyword = get_keyboard_text('', xbmc.getLocalizedString(14024), False)
+    if keyword:
+        log(f'Searching for location: {keyword}')
+        found_stations = find_station_by_name(keyword)
+        labels = [s[1] for s in found_stations]
+        if labels:
+            selected = dialog_select('Choose a location...', labels)
+            log(f'Selected: {selected}')
+            if selected != -1:
+                selected_location = found_stations[selected]
+                log(f'Selected location: {selected_location}')
+                ADDON.setSetting('Location{0}'.format(
+                    location_no), selected_location[1])
+                ADDON.setSetting('Location{0}ID'.format(
+                    location_no), selected_location[0])
+        else:
+            xbmcgui.Dialog().ok('Station not found', 'Could not find a station like that')
+
+
+def dialog_select(heading, _list, **kwargs):
+    return xbmcgui.Dialog().select(heading, _list, **kwargs)
+
+
+def get_keyboard_text(line='', heading='', hidden=False):
+    """
+    Source: https://github.com/vlmaksime/weather.gismeteo/blob/master/weather.gismeteo/resources/libs/__init__.py
+    """
+    kbd = xbmc.Keyboard(line, heading, hidden)
+    kbd.doModal()
+    if kbd.isConfirmed():
+        return kbd.getText()
+    return ''
 
 
 ########################################################################################
 # Main Kodi entry point
 ########################################################################################
-log(
-    f'version {ADDON.getAddonInfo("version")} started with argv: {sys.argv[1]}')
-
-fetch_weather_data()
-fetch_weather_alerts()
-
-set_property('Location1', 'Bramsche')
-set_property('Locations', 1)
-set_property('Forecast.IsFetched', 'true')
-set_property('Current.IsFetched', 'true')
-# set_property('Today.IsFetched', '')
-# set_property('Daily.IsFetched', 'true')
-# set_property('Detailed.IsFetched', 'true')
-# set_property('Weekend.IsFetched', '')
-# set_property('36Hour.IsFetched', '')
-# set_property('Hourly.IsFetched', 'true')
-set_property('DWD.IsFetched', 'true')
-set_property('WeatherProvider', 'DWD')
-set_property('WeatherProviderLogo', xbmcvfs.translatePath(os.path.join(
-    ADDON.getAddonInfo('path'), 'resources', 'media', 'dwd-logo-png.png')))
+log(f'{ADDON.getAddonInfo("name")} version {ADDON.getAddonInfo("version")} started with argv: {sys.argv}')
+if sys.argv[1] == 'find_location':
+    log('find_location: ' + sys.argv[2])
+    find_location(sys.argv[2])
+elif sys.argv[1] == '1':
+    fetch_weather_data()
+    for count, name in enumerate(get_station_names(), start=1):
+        set_property(f'Location{count}', name)
+    set_property('Locations', len(get_station_names()))
+    set_property('Forecast.IsFetched', 'true')
+    set_property('Current.IsFetched', 'true')
+    set_property('Today.IsFetched', 'true')
+    set_property('Daily.IsFetched', 'true')
+    # set_property('Detailed.IsFetched', 'true')
+    # set_property('Weekend.IsFetched', '')
+    # set_property('36Hour.IsFetched', '')
+    # set_property('Hourly.IsFetched', 'true')
+    set_property('DWD.IsFetched', 'true')
+    set_property('WeatherProvider', 'DWD')
+    set_property('WeatherProviderLogo', xbmcvfs.translatePath(os.path.join(
+        ADDON.getAddonInfo('path'), 'resources', 'media', 'dwd-logo-png.png')))
+else:
+    log('Unsupported command line argument!', xbmc.LOGERROR)
